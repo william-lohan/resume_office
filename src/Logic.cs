df8262da28dd3e80fd1e21dd80404d314899770d
William Lohan <william.g.lohan@gmail.com>
2022-06-21 22:50:13 -0700

    add resume animation

--- src/Logic.cs
+++ src/Logic.cs
@@ -8,17 +8,52 @@ public class Logic : Spatial
 
     AudioStreamPlayer3D resumeAudio;
 
+    AnimationPlayer resumeAnimationPlayer;
+
+    AnimationPlayer resumeAnimationPlayer2;
+
+    int step = 0;
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
         resumeAudio = GetNode<AudioStreamPlayer3D>("ResumeArea/AudioStreamPlayer3D");
+        resumeAnimationPlayer = GetNode<AnimationPlayer>("ResumeAnimationPlayer");
+        resumeAnimationPlayer2 = GetNode<AnimationPlayer>("ResumeAnimationPlayer2");
     }
 
     public void _OnResumeClick(InputEvent inputEvent, Vector3 position)
     {
-        // TODO show resume
         resumeAudio.Play();
+        switch(step) {
+            case 0:
+            {
+                resumeAnimationPlayer.Play("View0");
+                step++;
+                break;
+            }
+            case 1:
+            {
+                resumeAnimationPlayer.Play("Replace0");
+                resumeAnimationPlayer2.Play("View1");
+                step++;
+                break;
+            }
+            case 2:
+            {
+                resumeAnimationPlayer2.Play("Replace1");
+                step = 0;
+                break;
+            }
+            default:
+            {
+                resumeAnimationPlayer.Play("RESET");
+                resumeAnimationPlayer2.Play("RESET");
+                step = 0;
+                break;
+            }
+        }
     }
 
     public void _OnLaptopClick(InputEvent inputEvent, Vector3 position)
